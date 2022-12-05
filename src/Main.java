7b592aa476e5f8c6d23c0ca0402ded26a53b91ee
katiavasileva <eka.vasileva@gmail.com>
2022-12-05 22:57:46 +0500

    Task 1 is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//  Задача 1
+//  Данные сотрудников хранятся в неструктурированном формате, и бухгалтерия попросила написать программу,
+//  в которой можно работать с Ф.И.О. сотрудников. Напишите четыре строки:
+//  первая с именем firstName — для хранения имени;
+//  вторая с именем middleName — для хранения отчества;
+//  третья с именем lastName — для хранения фамилии;
+//  четвертая с именем fullName — для хранения Ф.И.О. сотрудника в формате "Фамилия Имя Отчество".
+//  Выведите в консоль фразу: “ФИО сотрудника — ….” В качестве данных для задачи используйте “Ivanov Ivan Ivanovich”.
+        System.out.println("Задача 1");
+        String firstName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName + ".");
+
+//  Задача 2
+//
     }
 }
